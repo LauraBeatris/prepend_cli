6a15293ac9d52bac7ba43aea0aeda3a3335cec9e
Laura <laurabeatriserafim@gmail.com>
2020-08-11 08:03:48 -0300

    feat: add function to stream prepend lines

--- lib/prepend_cli.ex
+++ lib/prepend_cli.ex
@@ -1,5 +1,9 @@
-defmodule PrependCLI do
-  def hello do
-    :world
+defmodule Prepend do
+  @moduledoc """
+  Manipulates the Prepend CLI logic
+  """
+
+  def stream_lines(lines, prefix) do
+    Stream.map(lines, &"#{prefix} #{&1}")
   end
 end
