3338654ab1bbafec3241375519803d0e5f7168f9
Laura <laurabeatriserafim@gmail.com>
2020-08-11 09:35:56 -0300

    chore: add doctest and spec to stream_lines function

--- lib/prepend_cli.ex
+++ lib/prepend_cli.ex
@@ -3,6 +3,17 @@ defmodule Prepend do
   Manipulates the Prepend CLI logic
   """
 
+  @doc """
+  Prepend a given string to each line of a list
+  and return a Stream with the itineration result
+
+  # Example:
+
+  iex> ["one", "two"] |> Prepend.stream_lines("$ ") |> Enum.to_list
+  ["$ one", "$ two"]
+  """
+
+  @spec stream_lines(Enumerable.t(), String.t()) :: Enumerable.t()
   def stream_lines(lines, prefix) do
     Stream.map(lines, &(prefix <> &1))
   end
