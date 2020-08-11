85b9f788f5f3907cd758322b0e8dfb3eff56358e
Laura <laurabeatriserafim@gmail.com>
2020-08-11 10:24:27 -0300

    refactor: improve tests

--- test/prepend_test.exs
+++ test/prepend_test.exs
@@ -1,5 +1,19 @@
 defmodule PrependTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  use ExUnitProperties
+
+  doctest Prepend
+
+  test "prepend string to lines" do
+    expected = ["R$ one", "R$ two", "R$ three"]
+
+    received =
+      ["one", "two", "three"]
+      |> Prepend.stream_lines("R$ ")
+      |> Enum.to_list()
+
+    assert expected == received
+  end
 
   test "handles multiple words" do
     expected = ["I like ice cream", "I like pizza", "I like cats"]
@@ -10,5 +24,18 @@ defmodule PrependTest do
       |> Enum.to_list()
 
     assert received == expected
+  end
+
+  @tag timeout: 300_000
+  property "streams" do
+    check all(
+            lines <- list_of(string(:printable)),
+            prefix <- string(:printable),
+            max_runs: 20
+          ) do
+      lines
+      |> Prepend.stream_lines(prefix)
+      |> Stream.each(&String.starts_with?(&1, prefix))
+    end
   end
 end
