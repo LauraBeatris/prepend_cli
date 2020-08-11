1b62dcefa2907884e1462dff6ca91c79bad09024
Laura <laurabeatriserafim@gmail.com>
2020-08-11 08:48:32 -0300

    chore: add tests

---
+++ test/prepend_test.exs
@@ -0,0 +1,10 @@
+test "handles multiple words" do
+  expected = ["I like ice cream", "I like pizza", "I like cats"]
+
+  received =
+    ["ice cream", "pizza", "cats"]
+    |> Prepend.stream_lines("I like ")
+    |> Enum.to_list()
+
+  assert received == expected
+end
