470b46961e2892398a0a283cd8810be414ff0986
Laura <laurabeatriserafim@gmail.com>
2020-08-12 09:11:55 -0300

    refactor: modify main prepend module

---
+++ lib/prepend.ex
@@ -0,0 +1,18 @@
+defmodule Prepend do
+  @moduledoc false
+
+  @doc """
+  Prepend a given string to each line of a list
+  and return a Stream with the itineration result
+
+  # Example:
+
+  iex> ["one", "two"] |> Prepend.stream_lines("$ ") |> Enum.to_list
+  ["$ one", "$ two"]
+  """
+
+  @spec stream_lines(Enumerable.t(), String.t()) :: Enumerable.t()
+  def stream_lines(lines, prefix) do
+    Stream.map(lines, &(prefix <> &1))
+  end
+end
