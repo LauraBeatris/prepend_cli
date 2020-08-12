8a2cc6279e4a96b858838d4d7e89550088c2a447
Laura <laurabeatriserafim@gmail.com>
2020-08-12 09:11:39 -0300

    chore: add cli module

---
+++ lib/prepend/cli.ex
@@ -0,0 +1,18 @@
+defmodule Prepend.CLI do
+  @moduledoc """
+  Prepend is a command line utility to prepend a given string to lines
+
+  ./prepend '$ ' < file.txt
+  """
+
+  def main(lines \\ []) do
+    IO.puts :stdio
+
+    output = IO.stream(:stdio, :line)
+
+    output
+    |> Prepend.stream_lines(Enum.join(lines, " "))
+    |> Stream.into(output)
+    |> Stream.run()
+  end
+end
